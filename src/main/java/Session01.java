0b8309e395674fb355ba1b2d23ac94f10a184816
Usuario <Usuario@Desktop>
2026-04-24 11:39:24 -0500

    Se completo la funcionalidad MVC

--- src/main/java/Session01.java
+++ src/main/java/Session01.java
@@ -1,9 +1,18 @@
 
+import controlador.ControladorOperaciones;
 import java.util.Scanner;
 import modelo.Operaciones;
+import vista.frmOperaciones;
 public class Session01 {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+        
+        Operaciones modelo = new Operaciones(20, 30);
+        frmOperaciones vista =  new frmOperaciones();
+        ControladorOperaciones controlador = 
+                    new ControladorOperaciones(modelo, vista);
+        controlador.iniciar();
+        
+        /*Scanner teclado = new Scanner(System.in);
         Operaciones operacion = new Operaciones();
         System.out.print("Ingrese primer valor :");
         int numero = teclado.nextInt();
@@ -14,5 +23,10 @@ public class Session01 {
         System.out.println("La suma es " + operacion.sumar());
         System.out.println("La resta es " + operacion.restar());
         System.out.println("El producto es " + operacion.multiplicar());
+        */
+    }
+
+    private static frmOperaciones frmOperaciones() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
